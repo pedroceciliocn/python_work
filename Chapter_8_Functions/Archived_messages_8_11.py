216851d3b32aa01ae27f9a9ed53711735f1c95d5
pedroceciliocn <pedroceciliocn@gmail.com>
2021-01-02 13:12:35 -0300

    chapter 8 about functions first files added

---
+++ Chapter_8_Functions/Archived_messages_8_11.py
@@ -0,0 +1,32 @@
+#Sending Messages:
+def send_messages(messages, sent_messages):
+	"""
+	Simulate printing each message, until none are left.
+	Move each message to sent_messages after printing.
+	"""
+	while messages:
+		current_message = messages.pop()
+		print(f"Printing message: {current_message}")
+		sent_messages.append(current_message)
+
+def show_messages(sent_messages):
+	"""Show all the messages that were printed."""
+	print("\nThe following messages have been printed:")
+	for sent_message in sent_messages:
+		print(sent_message)
+
+messages = ['a', 'b', 'c', 'd', 'vsf']
+sent_messages = []
+
+##send_messages(messages, sent_messages)
+
+#Preventing a function from modifying a list
+#function_name(list_name[:])
+send_messages(messages[:], sent_messages)
+
+show_messages(sent_messages)
+
+print(messages)
+print(sent_messages)
+
+
---
+++ Chapter_8_Functions/Messages_8_9.py
@@ -0,0 +1,9 @@
+messages = ['a', 'b', 'c', 'd', 'vsf']
+
+def show_messages(msgs):
+	"""Print a msg from a list"""
+	for msg in msgs:
+		print(msg)
+
+
+show_messages(messages)
