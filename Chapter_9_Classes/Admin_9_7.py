63fbd8665c2522faff6c6fac337715de0b73cc4f
pedroceciliocn <pedroceciliocn@gmail.com>
2021-01-07 15:33:49 -0300

    more exercises added

---
+++ Chapter_9_Classes/Admin_9_7.py
@@ -0,0 +1,45 @@
+#Admin:
+class User:
+	def __init__(self, first_name, last_name, user_age, user_location):
+		self.f_name = first_name
+		self.l_name = last_name   #try later to unite both names in a same parameter
+		self.age = user_age
+		self.location = user_location
+		self.login_attempts = 0
+
+	def describe_user(self):
+		print(f"Name: {self.f_name.title()} {self.l_name.title()}.")
+		print(f"Age: {self.age} years old.")
+		print(f"Location: {self.location.title()}.")
+
+	def greet_user(self):
+		print(f"Hello {self.f_name.title()} {self.l_name.title()} from {self.location.title()}, you're welcome!")
+
+	def increment_login_attempts(self):
+		self.login_attempts = self.login_attempts + 1
+
+	def reset_login_attempts(self):
+		self.login_attempts = 0
+
+class Admin(User):
+	def __init__(self, first_name, last_name, user_age, user_location):
+		"""
+		Initialize attributes of the parent class.
+		Then initialize attributes specific to an admin user.
+		"""
+		super().__init__(first_name, last_name, user_age, user_location)
+		self.privileges = ['can add post', 'can delete post', 'can ban user']
+
+	def show_privileges(self):
+		print("The admin privileges are: ")
+		for privilege in self.privileges:
+			print(privilege)
+
+
+user_0 = User('pedro', 'neto', 26, 'petrolina')
+user_1 = User('gabriel', 'cruz', 24, 'petrolina')
+user_2 = User('artur', 'cruz', 25, 'recife')
+
+adm = Admin('edward', 'elwric', 8, 'petrolina')
+
+adm.show_privileges()
---
+++ Chapter_9_Classes/Ice_Cream_Stand_9_6.py
@@ -0,0 +1,44 @@
+#Ice Cream Stand:
+class Restaurant:
+	"""A simple attempt to model a restaurant."""
+	def __init__(self, restaurant_name, cuisine_type):
+		self.name = restaurant_name
+		self.type = cuisine_type
+		self.number_served = 0
+
+	def describe_restaurant(self):
+		print(f"The restaurant is called {self.name}.")
+		print(f"The cuisine type is {self.type}.")
+
+	def open_restaurant(self):
+		print(f"The {self.name} restaurant is now open.")
+
+	def set_number_served(self, served):
+		self.number_served = served
+
+	def increment_number_served(self, atm_served):
+		self.number_served += atm_served
+
+class IceCreamStand(Restaurant):
+	""" ."""
+	def __init__(self, restaurant_name, cuisine_type):
+		"""
+		Initialize attributes of the parent class.
+		Then initialize attributes specific to an ice cream stand.
+		"""
+		super().__init__(restaurant_name, cuisine_type)
+		self.flavors = ['chocolate', 'vanilla', 'manga', 'lemon', 'flakes']
+
+	def list_icecream_flavors(self):
+		#print(f"The flavors of ice cream available are: {self.flavors}")
+		print("The flavors of ice cream available are: ")
+		for flavor in self.flavors:
+			print(flavor.title())
+
+
+
+barraca_sorvete = IceCreamStand('Zecas', 'Sorveteria')
+#restaurant.set_number_served(20)
+#restaurant.increment_number_served(10)
+#print(restaurant.number_served)
+barraca_sorvete.list_icecream_flavors()
