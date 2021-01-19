5b518d5affeac07a00e68c336bf7a79f8c2c617f
Pedro Neto <pedroceciliocn@gmail.com>
2021-01-18 21:07:15 -0300

    chpter 15 about data viz started

---
+++ Chapter_15_Generating_Data/15_1/Cubes.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+
+x_values = range(1, 5)
+y_values = [x**3 for x in x_values]
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=100)
+
+# Set chart title and label axes.
+ax.set_title("Cube Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
+
+# Set size of tick labels.
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the range for each axis.
+#ax.axis([0, 1100, 0, 1100000])
+
+plt.show()
+
+###############################################################################
+import matplotlib.pyplot as plt
+
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=1)
+
+# Set chart title and label axes.
+ax.set_title("Cube Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
+
+# Set size of tick labels.
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the range for each axis.
+#ax.axis([0, 1100, 0, 1100000])
+
+plt.show()
