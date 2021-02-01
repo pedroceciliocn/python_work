695a5b947818e1024125ceba83da49164a737501
pedroceciliocn <pedroceciliocn@gmail.com>
2021-02-01 17:11:27 -0300

    more files added

--- Chapter_18_Getting_Started_With_Django/learning_log/learning_logs/views.py
+++ Chapter_18_Getting_Started_With_Django/learning_log/learning_logs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Topic
+
+def index(request):
+	"""The home page for Learning Log."""
+	return render(request, 'learning_logs/index.html')
+
+def topics(request):
+	"""Show all topics."""
+	topics = Topic.objects.order_by('date_added')
+	context = {'topics': topics}
+	return render(request, 'learning_logs/topics.html', context)
